d7ec88f587cfa856ea529ea31cdde6a5331aae9c
feelpass <youngpil.lee@gmail.com>
2023-09-14 22:58:25 +0900

    init

---
+++ src/test/kotlin/com/example/profile/ProfileApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.example.profile
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProfileApplicationTests {
+    @Test
+    fun contextLoads() {
+
+    }
+
+}
